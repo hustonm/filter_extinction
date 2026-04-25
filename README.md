e7c5e036cc0753c69ec87e2b7dd554ba05d70541
Macy Huston <mhuston@berkeley.edu>
2026-04-24 17:17:46 -0700

    app mag corr estimator progress

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ The extinction law changes dramatically over this range, so the extinction in th
 is strongly dependent on its spectrum and the absolute extinction. Galactic models typically apply a constant extinction coefficient
 using some effective wavelength to
 scale A\_Ks or some other absolute extinction quantity to that of each individual filter. Here, we use model spectra and filter integration
-to create a more accurate estimate of the extinction coefficient as a function of A\_Ks and stellar colors.
+to create a more accurate estimate of the extinction coefficient as a function of A\_Ks and stellar colors. For convenience we provide corrections the other Roman filters and for both absolute and apparent colors with different observed filter combinations.
--- extinction_estimator.py
+++ extinction_estimator.py
@@ -9,6 +9,10 @@ with open('roman_fits_abs_AKs5.json', 'r') as f:
     roman_fits_abs_AKs5 = json.load(f)
 with open('roman_fits_abs_AKs1.json', 'r') as f:
     roman_fits_abs_AKs1 = json.load(f)
+with open('roman_fits_app_AKs5.json', 'r') as f:
+    roman_fits_app_AKs5 = json.load(f)
+with open('roman_fits_app_AKs1.json', 'r') as f:
+    roman_fits_app_AKs1 = json.load(f)
 
 def generic_extinction_polynomial(AKs_C, coeffs, order):
     """
@@ -95,5 +99,66 @@ def get_roman_extinction_sim(catalog, low_extinction=False):
         
         ext_filt = generic_extinction_polynomial(AKs_C, coeffs, order)
         result[filt] = ext_filt
+        
+    return result
+              
+
+              
+def get_roman_extinction_obs(catalog, low_extinction=False, nans=None):
+    """
+    Roman extinction estimator for observations. Assumes all Roman filter photometry 
+    is provided and in apparent AB mags.
+    
+    Parameters:
+    -----------
+    catalog : pd.DataFrame, astropy.table.Table, or similar
+        required columns: A_Ks, f062, f087, f106, f129, f158, f184, f213, and f146, where 
+        nan for missing magnitudes are ok (but may result in some nan extinctions)
+    low_extinction=False : boolean
+        if all A_Ks<=1, use the alternate lower order correction. if any A_Ks>1, a warning 
+        will be printed, and the higher order correction will be used
+    nans=None : float or int
+        fill value that represents nans in the catalog. if none, assumes any NaNs are 
+        already represented by np.nan. NaN results will be returned with this fill value too.
+        
+    Returns:
+    --------
+    extinctions : dict
+        entries of '<filter>':[<ext_star1>, <ext_star2>, ...] for each filter
+    """
+    # Select the appropriate fit_dict
+    fit_dict_all = roman_fits_abs_AKs5
+    if low_extinction and np.all(catalog['A_Ks']<=1):
+        fit_dict_all = roman_fits_abs_AKs1
+    elif low_extinction:
+        warnings.warn("low_extinction set to True, but some A_Ks > 1. "
+                      "switching to 0 <= A_Ks <= 5 fit.")
+    
+    # Iterate over the filters for v1 corrections
+    filt_dict = filt_dict_all['v1']
+    result = {}
+    any_nans = False
+    for filt in roman_filter_list:
+        filt_fit = fit_dict[filt]
+        colors = filt_fit['colors']
+        coeffs = filt_fit['coefficients']
+        order = filt_fit['order']
+        print(f"estimating {filt} extinction using {colors} and order={order} function"
+
+        columns = [catalog['A_Ks']]
+        for c in colors:
+            f1,f2 = c.split('_')
+            columns.append(catalog[f1]-catalog[f2])
+        AKs_C = np.stack(columns)
+        
+        ext_filt = generic_extinction_polynomial(AKs_C, coeffs, order)
+        result[filt] = ext_filt
+        if np.any(np.isnan(ext_filt)):
+              any_nans = True
+    # Return these if there are no nans, otherwise continue to alternate correcions
+    if not any_nans:
+        return result
+              
+    # TODO put in alternate versions for missing photometry
         
     return result
